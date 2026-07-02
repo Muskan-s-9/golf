057f05c132147f507e1aab01412471f606e5de02
Muskan Sonkar <87746861+Muskan-s-9@users.noreply.github.com>
2026-07-02 19:59:34 +0530

    Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -197,7 +197,7 @@ class WinnerPayoutPayload(BaseModel):
 app = FastAPI(title="Golf Impact Platform", version="1.0.0")
 origins = [
     "http://localhost:5173",
-    "https://golf-frontend-hxiqhmyl3-muskan090s-projects.vercel.app"
+    "golf-frontend-ashy.vercel.app"
 ]
 app.add_middleware(
     CORSMiddleware,
